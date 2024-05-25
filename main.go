a9dc1b282fdb6ca13b39ccad474453679475145d
Bolmy <49241877+Bolmy@users.noreply.github.com>
2024-05-25 13:08:58 +0200

    Remove depricated code

--- main.go
+++ main.go
@@ -2,24 +2,15 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"os"
-	"time"
 )
-
-const internalCommunicationChannel = "127.0.0.1:20202"
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("start <My\\Path\\To\\Config>|listen|check|end")
+		fmt.Println("MQTTToDB <My\\Path\\To\\Config>")
 		os.Exit(1)
 	}
 
 	core(os.Args[1])
 
-}
-
-func actionLoop() string {
-	time.Sleep(100 * time.Millisecond)
-	return fmt.Sprintf("%d\n", rand.Intn(1000))
 }
